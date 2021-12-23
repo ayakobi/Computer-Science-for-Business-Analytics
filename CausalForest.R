1a006aefe9fd5f246c6fd7e310077341e9a85b08
ayakobi <91407486+ayakobi@users.noreply.github.com>
2021-12-23 13:14:26 +0100

    Update CausalForest.R

--- CausalForest.R
+++ CausalForest.R
@@ -1,9 +1,18 @@
+#READ ME: 
+# Hyper-contextual targeting has been a popular and effective advertisement tool due to global digital-
+# ization. To enhance customer responses to marketing campaigns, companies have been collecting data
+# and exploiting not only customer characteristics but also environmental factors. The main contextual
+# factor that is analyzed in this research, is crowdedness in public transport. 
+# variations in crowdedness on purchase likelihood, the Causal Forest algorithm is implemented (Athey, Tibshirani, Wager, et al. 2019). 
+# This Machine Learning technique estimates heterogeneous treatment effects and allows for statistical inference.
+# This research constructs a simulation framework that models customer heterogeneity and the non-linear effects of crowdedness.
+# Below, you can find the implementation of the simulation framework and the Casual Forest from the grf-package in R.
+
+
+
 #Installing of Packages
 
-#Abdelmounaim el Yaakoubi [50979ay]
-#Note: this code is still subject to changes
-
-#Import the datasets 'Dataset1' and 'Data2' from the zip-file povided by INFROMS
+#Import the datasets 'Dataset1' and 'Data2' from the zip-file povided by INFORMS
 #Install packages
 install.packages("tidyverse")
 library(tidyverse)
